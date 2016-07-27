54ae5f8d23d0aeaec0245a5cd3c0c668e28951e4
NMND <reddigitalsw@gmail.com>
2016-07-27 17:26:58 +0300

    Create players queue

--- crossnullsCtrl.js
+++ crossnullsCtrl.js
@@ -5,8 +5,27 @@
   .controller('crossnullsCtrl', ['$scope', crossnullsCtrl]);
 
   function crossnullsCtrl($scope) {
-     $scope.cells = function() {
-
-     };
+    console.log('Crossnulls controller is working...');
+    $scope.field = [  //Cells Array
+      {cell: '1'},
+      {cell: '2'},
+      {cell: '3'},
+      {cell: '4'},
+      {cell: '5'},
+      {cell: '6'},
+      {cell: '7'},
+      {cell: '8'},
+      {cell: '9'}
+    ];
+    console.log('Added array with cells...');
+    console.log('Turn = ' + turn + ' - waiting for x turn');
+/*    while (turn === 0) {
+      console.log('Waiting for X-player turn...');
+      break;
+    }
+    while (turn === 1) {
+      console.log('Waiting for O-player turn...')
+      break;
+    }*/
   };
 })();
--- myCellCtrl.js
+++ myCellCtrl.js
@@ -5,20 +5,40 @@
   .controller('myCellCtrl', ['$scope', myCellCtrl]);
 
   function myCellCtrl($scope) {
-    var turn = true;
-    console.log('CellCtrl is working...');
-    if (turn == true) {
+  /*  if (turn == true) {
     $scope.clickQueue = function() {
         $scope.state = "x";
-        turn != true;
+        turn = false;
+        console.log('turn=' + turn);
       }
-    }
-    else {
+    };
+    if (turn == false) {
     $scope.clickQueue = function() {
         $scope.state = "o";
         turn = true;
+        console.log('turn=' + turn);
       }
-    }
-  };
-
+    };*/
+    var filled = false;
+      $scope.clickQueue = function() {
+        if (turn === 0) {
+          if (filled == false) {
+          $scope.state = "o";
+          turn = 1;
+          filled = true;
+          console.log('Turn = ' + turn);
+          console.log('Cell is filled = ' + filled);
+          }
+        }
+      else {
+        if (filled == false) {
+          $scope.state = "x";
+          turn = 0;
+          filled = true;
+          console.log('Turn = ' + turn);
+          console.log('Cell is filled = ' + filled);
+          }
+        };
+      };
+    };
 })();
