cc4b3e95d299ab7abdf66174c181eb4135518ae0
NMND <reddigitalsw@gmail.com>
2016-07-30 04:27:16 +0300

    Add win logic

--- crossnullsCtrl.js
+++ crossnullsCtrl.js
@@ -7,7 +7,8 @@
   function crossnullsCtrl($scope) {
     console.log('Crossnulls controller is working...');
     console.log('Added array with cells...');
-    console.log('Turns left = ' + turnsLeft);
+    console.log(states);
     console.log('Game is started...');
+    console.log('Turns left = ' + turnsLeft);
   };
 })();
--- myCellCtrl.js
+++ myCellCtrl.js
@@ -8,6 +8,7 @@
     var filled = false;
     var track;
       $scope.clickQueue = function() {
+        if( win !== true) {
         switch (turn) {
           case 0:
           if (filled == false) {
@@ -18,6 +19,8 @@
           console.log('Turn = ' + turn);
           console.log('Turns left = ' + turnsLeft);
           console.log($scope.state + ' on ' + ($scope.cell));
+          states[$scope.$index] = $scope.state;
+          console.log(states);
         }
       case 1 :
         if (filled == false) {
@@ -29,8 +32,61 @@
           console.log('Turns left = ' + turnsLeft);
           console.log($scope.state + ' on ' + ($scope.cell));
     //      console.log('Cell is filled = ' + filled);
+          states[$scope.$index] = $scope.state;
+          console.log(states);
           }
         };
+      }
+        function checkWin(){
+          if (win !== true){
+          if ((states[0] == states[1]) && (states[0] == states[2]) == true) {
+            console.log(states[0] + ' WIN!');
+            winLine = {"top": "54px", "bottom":"auto"};
+            win = true;
+          }
+          if ((states[3] == states[4]) && (states[3] == states[5]) == true) {
+            console.log(states[3] + ' WIN!');
+            winLine = {"top": "0", "bottom":"0"};
+            win = true;
+          }
+          if ((states[6] == states[7]) && (states[6] == states[8]) == true) {
+            console.log(states[6] + ' WIN!');
+            winLine = {"top": "auto", "bottom":"54px"};
+            win = true;
+          }
+          if ((states[0] == states[3]) && (states[0] == states[6]) == true) {
+            console.log(states[0] + ' WIN!');
+            winLine = {"left":"-184px","right":"auto", "transform":"rotate(90deg)"};
+            win = true;
+          }
+          if ((states[1] == states[4]) && (states[1] == states[7]) == true) {
+            console.log(states[1] + ' WIN!');
+            winLine = {"left":"-80px","right":"0", "transform":"rotate(90deg)"};
+            win = true;
+          }
+          if ((states[2] == states[5]) && (states[2] == states[8]) == true) {
+            console.log(states[2] + ' WIN!');
+            winLine = {"left":"auto","right":"-184px", "transform":"rotate(90deg)"};
+            win = true;
+          }
+          if ((states[0] == states[4]) && (states[0] == states[8]) == true) {
+            console.log(states[0] + ' WIN!');
+            winLine = {"transform":"rotate(45deg)"};
+            win = true;
+          }
+          if ((states[2] == states[4]) && (states[4] == states[6]) == true) {
+            console.log(states[2] + ' WIN!');
+            winLine = {"transform":"rotate(-45deg)"};
+            win = true;
+          }
+        };
+        if ((turnsLeft == 0) && (win !== true)) {
+          console.log('Nobody wins...Click on "Restart" to try again.')
+          win = true;
+          winLine = {"opacity":"0"};
+        }
+        };
+        checkWin();
       };
     };
 })();
